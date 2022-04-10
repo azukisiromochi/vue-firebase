1f1f8a91d8337706ac901b5a654cdf8bd860aa37
azukisiromochi <azukisiromochi@gmail.com>
2022-04-10 23:07:29 +0900

    ✨ Firebase の導入

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,18 @@
 </template>
 
 <script>
+import { initializeApp } from "firebase/app"
 import HelloWorld from './components/HelloWorld.vue'
+
+const config = {
+  apiKey: "AIzaSyCb4eKf6YFgicsy0_g7-hqBZ9e6xSanUXk",
+  authDomain: "vue-firebase-sample-a6587.firebaseapp.com",
+  databaseURL: "https://vue-firebase-sample-a6587.firebaseio.com",
+  projectId: "vue-firebase-sample-a6587",
+  storageBucket: "vue-firebase-sample-a6587.appspot.com",
+  messagingSenderId: "959054759724",
+}
+initializeApp(config)
 
 export default {
   name: 'App',
